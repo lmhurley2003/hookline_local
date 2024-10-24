a701df27b5b3450a4b6303d4fb75f01530c85e8b
Jude Markabawi <34716142+judemarkabawi@users.noreply.github.com>
2024-10-24 17:18:36 -0400

    Windows build fixes?

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include "core/Application.hpp"
 
-int main() {
+int main(int argc, char** argv) {
+    (void) argc;
+    (void) argv;
+
     Application app;
     app.init();
     app.run();
+
+    return 0;
 }
